3392a6b483bbd334f4fa5e6a6dffed825464c709
Dave Macaulay <dmacaulay@magento.com>
2017-08-17 15:24:31 +0200

    Add support for uiClass

--- index.d.ts
+++ index.d.ts
@@ -16,3 +16,58 @@ interface MageTranslate {
 }
 
 declare var $t: MageTranslate;
+declare module 'mage/translate' {
+    export = $t;
+}
+
+/**
+ * Support for uiClass
+ */
+interface MageUiClass {
+    defaults: MageUiClassDefaults;
+
+    /**
+     * Create new instance of UI class
+     * @param args
+     */
+    new(...args: Array<any>): MageUiClass;
+
+    /**
+     * Entry point to the initialization of consturctors' instance.
+     *
+     * @param {Object} [options={}]
+     * @returns {Class} Chainable.
+     */
+    initialize(options: object): MageUiClass;
+
+    /**
+     * Recursively extends data specified in constructors' 'defaults'
+     * property with provided options object. Evaluates resulting
+     * object using string templates (see: mage/utils/template.js).
+     *
+     * @param {Object} [options={}]
+     * @returns {Class} Chainable.
+     */
+    initConfig(options: object): MageUiClass;
+
+    /**
+     * Creates new constructor based on a current prototype properties,
+     * extending them with properties specified in 'exender' object.
+     *
+     * @param {Object} [extender={}]
+     * @returns {Function} New constructor.
+     */
+    extend(extender: MageUiClass): MageUiClass;
+}
+
+interface MageUiClassDefaultIgnoreTmpls {
+    templates: boolean;
+}
+interface MageUiClassDefaults {
+    ignoreTmpls: MageUiClassDefaultIgnoreTmpls;
+}
+
+declare var uiClass: MageUiClass;
+declare module 'uiClass' {
+    export = uiClass;
+}
